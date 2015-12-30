71ef60f6a3821e9ff34ca9c6a5d09abe2f8779d6
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2015-12-30 19:12:50 -0200

    Pack and send initial styles before entire app is loaded

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app/app.js',
+  entry: {
+    app: './app/app.js',
+    styles: './app/styles/styles.js'
+  },
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'app.js'
+    filename: '[name].js'
   },
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
+    }, {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
     }]
   }
 };
